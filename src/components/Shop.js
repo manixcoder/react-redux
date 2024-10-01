c109e10028e328ff3261bec1faa972b342c46a6a
manixcoder <pathakmanish86@gmail.com>
2024-10-02 01:21:30 +0530

    My First comment

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+      <h2>Buy Adidas Shoes 50 Rs</h2>
+      <botton className="btn btn-primary mx-2">-</botton>
+      Add to card
+      <botton className="btn btn-primary mx-2">+</botton>
+    </div>
+  )
+}
+
+export default Shop
